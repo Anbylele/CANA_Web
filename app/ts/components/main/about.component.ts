bbfdfc5f83b5488742fcc7c32b556a46df28a659
Zhouyang Ye <zye0821@gmail.com>
2017-06-05 01:19:37 -0400

    news component header component and about component

--- app/ts/components/main/about.component.ts
+++ app/ts/components/main/about.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild , HostListener} from '@angular/core';
+import {listeners} from "cluster";
 
 @Component({
     moduleId: module.id,
@@ -8,6 +9,49 @@ import { Component } from '@angular/core';
 })
 
 export class AboutComponent{
+    @ViewChild("mentorList") mentorList: ElementRef;
+
+    private oMentorList: any;
+    private mentorIntro:any;
+    private mentorImage: any;
+    private mentor: any;
+
     constructor(){
+    }
+
+    ngAfterViewInit(): void {
+        this.oMentorList = this.mentorList.nativeElement;
+        this.mentorIntro = this.oMentorList.querySelectorAll(".intro");
+        this.mentorImage = this.oMentorList.querySelectorAll(".img");
+        this.mentor = this.oMentorList.querySelectorAll(".mentor");
+
+        //handle mentor list mentor picture height
+        this.handleMentorIntroWidth();
+        this.handleMentorPicHeight();
+    }
+
+    @HostListener('window:resize', ['$event']) onResize(event): void{
+        this.handleMentorIntroWidth();
+        this.handleMentorPicHeight();
+    }
+
+    handleMentorPicHeight(): void {
+        for(let i=0;i<this.mentorIntro.length;i++) {
+            this.mentorImage[i].style.height = this.mentorIntro[i].offsetHeight + "px";
+        }
+    }
+
+    handleMentorIntroWidth(): void {
+        for(let i=0;i<this.mentorIntro.length;i++) {
+            let marginLeft = parseInt(window.getComputedStyle(this.mentorIntro[i]).marginLeft);
+            let marginRight = parseInt(window.getComputedStyle(this.mentorIntro[i]).marginRight);
+            let width = this.mentor[i].offsetWidth - parseInt(window.getComputedStyle(this.mentor[i]).paddingLeft) - parseInt(window.getComputedStyle(this.mentor[i]).paddingRight);
+            if(width < 470 + marginLeft + marginRight + this.mentorImage[i].offsetWidth) {
+                this.mentorIntro[i].style.width = (width - this.mentorImage[i].offsetWidth - marginLeft - marginRight - 2) + "px";
+                console.log("in");
+            }else {
+                this.mentorIntro[i].style.width = "470px"
+            }
+        }
     }
 }
