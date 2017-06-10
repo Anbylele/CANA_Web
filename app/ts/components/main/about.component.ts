620153765a2cae59803d39431ba8de81329cb856
Zhouyang Ye <zye0821@gmail.com>
2017-06-10 02:43:28 -0400

    scroll bar feature

--- app/ts/components/main/about.component.ts
+++ app/ts/components/main/about.component.ts
@@ -10,11 +10,16 @@ import {listeners} from "cluster";
 
 export class AboutComponent{
     @ViewChild("mentorList") mentorList: ElementRef;
+    @ViewChild("container") container: ElementRef;
+    @ViewChild("scroll") scroll: ElementRef;
+
 
     private oMentorList: any;
     private mentorIntro:any;
     private mentorImage: any;
     private mentor: any;
+    private oScroll: any;
+    private oContainer: any;
 
     constructor(){
     }
@@ -24,10 +29,17 @@ export class AboutComponent{
         this.mentorIntro = this.oMentorList.querySelectorAll(".intro");
         this.mentorImage = this.oMentorList.querySelectorAll(".img");
         this.mentor = this.oMentorList.querySelectorAll(".mentor");
+        this.oScroll = this.scroll.nativeElement;
+        this.oContainer = this.container.nativeElement;
 
         //handle mentor list mentor picture height
         this.handleMentorIntroWidth();
         this.handleMentorPicHeight();
+
+        //handle mobile mentor list drag
+        this.oMentorList.ontouchstart = (e) => {
+            this.onMobileDrag(e, this.oScroll, this.oContainer, this.oMentorList, 'content');
+        };
     }
 
     @HostListener('window:resize', ['$event']) onResize(event): void{
@@ -48,10 +60,165 @@ export class AboutComponent{
             let width = this.mentor[i].offsetWidth - parseInt(window.getComputedStyle(this.mentor[i]).paddingLeft) - parseInt(window.getComputedStyle(this.mentor[i]).paddingRight);
             if(width < 470 + marginLeft + marginRight + this.mentorImage[i].offsetWidth) {
                 this.mentorIntro[i].style.width = (width - this.mentorImage[i].offsetWidth - marginLeft - marginRight - 2) + "px";
-                console.log("in");
             }else {
                 this.mentorIntro[i].style.width = "470px"
             }
         }
+    }
+
+    onDrag(event, scrollBar, container, mentorList, type): void {
+        event.stopPropagation();
+        event.preventDefault();
+        let timer;
+        let init = {
+            initialY: event.screenY,
+            speed: 0,
+            change: event.screenY,
+            resistance: 1.1
+        };
+        let onMove, top, bottom;
+        if(type.toUpperCase() === "SCROLLBAR") {
+            onMove  = this.onMove;
+            top = scrollBar.offsetTop;
+            bottom = container.offsetHeight - scrollBar.offsetHeight;
+        }else if(type.toUpperCase() === "CONTENT") {
+            onMove = this.onContentMove;
+            top = mentorList.offsetTop;
+            bottom = mentorList.offsetHeight - container.offsetHeight;
+        }
+        let move = this.move;
+        document.addEventListener("mousemove", handleMove, false);
+        function handleMove(e){
+            onMove(e, top, bottom, init, scrollBar, container, mentorList);
+        }
+        window.addEventListener("mouseup", function handleUp(){
+            clearInterval(timer);
+            if(type.toUpperCase() === "CONTENT") {
+                timer = setInterval(() => {
+                    move(timer, bottom, init, scrollBar, container, mentorList);
+                },20);
+            }
+            document.removeEventListener("mousemove", handleMove, false);
+            window.removeEventListener("mouseup", handleUp, false);
+        }, false);
+    }
+
+    onMobileDrag(event, scrollBar, container, mentorList, type): void {
+        event.stopPropagation();
+        event.preventDefault();
+        let timer;
+        let e = event.touches[0];
+        let init = {
+            initialY: e.screenY,
+            speed: 0,
+            change: e.screenY,
+            resistance: 1.1,
+            triggered: false,
+            markY: 0,
+            scrollTop: document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop
+        };
+        let onMove, top, bottom;
+        if(type.toUpperCase() === "SCROLLBAR") {
+            onMove  = this.onMove;
+            init.speed = 0;
+            top = scrollBar.offsetTop;
+            bottom = container.offsetHeight - scrollBar.offsetHeight;
+        }else if(type.toUpperCase() === "CONTENT") {
+            onMove = this.onContentMove;
+            top = mentorList.offsetTop;
+            bottom = mentorList.offsetHeight - container.offsetHeight;
+        }
+        let move = this.move;
+        document.addEventListener("touchmove", handleMove, false);
+        function handleMove(ev){
+            onMove(ev, top, bottom, init, scrollBar, container, mentorList);
+        }
+        window.addEventListener("touchend", function handleUp(){
+            clearInterval(timer);
+            if(type.toUpperCase() === "CONTENT") {
+                timer = setInterval(() => {
+                    move(timer, bottom, init, scrollBar, container, mentorList);
+                },20);
+            }
+            document.removeEventListener("touchmove", handleMove, false);
+            window.removeEventListener("touchend", handleUp, false);
+        }, false);
+    }
+
+    move(timer, bottom, init, scrollBar, container, mentorList): void {
+        let Y = init.speed + mentorList.offsetTop;
+        if(Y > 0) {
+            Y = 0;
+            init.speed = 0;
+            clearInterval(timer);
+        }else if(Y < -bottom) {
+            init.speed = 0;
+            Y = -bottom;
+            clearInterval(timer);
+        }
+        init.speed = init.speed/init.resistance;
+        if(Math.abs(init.speed) < 1) {
+            clearInterval(timer);
+        }
+
+        let percent = Y / bottom;
+        let range = container.offsetHeight - scrollBar.offsetHeight;
+        mentorList.style.top = Y +  "px";
+        scrollBar.style.top = -(range * percent) + "px";
+    }
+
+    onMove(event, barTop, bottom, init, scrollBar, container, mentorList): void {
+        event.stopPropagation();
+        event.preventDefault();
+        let e = event.touches ? event.touches[0] : event;
+        let mouseY = e.screenY;
+        let moveY = mouseY - init.initialY;
+        init.speed = e.screenY - init.change;
+        init.change = e.screenY;
+        let Y = moveY + barTop;
+        if(Y < 0) {
+            Y = 0;
+        }else if(Y > bottom) {
+            Y = bottom;
+        }
+        let percent = Y / bottom;
+        let range = mentorList.offsetHeight - container.offsetHeight;
+        mentorList.style.top = -(range * percent) + "px";
+        scrollBar.style.top = Y + "px";
+    }
+
+    onContentMove(event, contentTop, bottom, init, scrollBar, container, mentorList): void {
+        if(!event.touches) {
+            event.stopPropagation();
+            event.preventDefault();
+        }
+        let e = event.touches ? event.touches[0] : event;
+        let mouseY = e.screenY;
+        let moveY = mouseY - init.initialY;
+        init.speed = e.screenY - init.change;
+        init.change = e.screenY;
+        let Y = moveY + contentTop;
+        if(Y >= 0) {
+            Y = 0;
+        }else if(Y <= -bottom) {
+            Y = -bottom;
+        }else {
+            init.triggered = false;
+        }
+        if (Y === 0 || Y === -bottom){
+            if(event.touches){
+                if(!init.triggered) {
+                    init.markY = e.screenY;
+                    init.triggered = true;
+                    init.scrollTop = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+                }
+                document.documentElement.scrollTop = init.scrollTop - e.screenY + init.markY;
+                document.body.scrollTop = init.scrollTop - e.screenY + init.markY;
+            }
+        }
+        let percent = Y / bottom;
+        let range = container.offsetHeight - scrollBar.offsetHeight;
+        mentorList.style.top = Y +  "px";
+        scrollBar.style.top = -(range * percent) + "px";
     }
 }
--- app/ts/components/main/news.component.ts
+++ app/ts/components/main/news.component.ts
@@ -10,10 +10,15 @@ import { AppUtil } from '../../util/app.util';
 
 export class NewsComponent{
     @ViewChild("nav") nav:ElementRef;
+    @ViewChild("cnav") cnav:ElementRef;
 
     private oNav: any;
     private aButtons: any;
     private aUnderscores: any;
+
+    private oCNav: any;
+    private aCButtons: any;
+    private aCUnderscores: any;
 
     constructor(
         private appUtil:AppUtil
@@ -24,10 +29,18 @@ export class NewsComponent{
         this.aButtons = this.oNav.querySelectorAll("button");
         this.aUnderscores = this.oNav.querySelectorAll(".underscore");
 
+        this.oCNav = this.cnav.nativeElement;
+        this.aCButtons = this.oCNav.querySelectorAll("button");
+        this.aCUnderscores = this.oCNav.querySelectorAll(".underscore");
+
         //initialize nav bar
         for(let i=0;i<this.aButtons.length;i++) {
             this.aUnderscores[i].style.width = "0";
             this.aUnderscores[i].style.left = (this.aButtons[i].offsetLeft + this.aButtons[i].offsetWidth / 2) + "px";
+        }
+        for(let i=0;i<this.aCButtons.length;i++) {
+            this.aCUnderscores[i].style.width = "0";
+            this.aCUnderscores[i].style.left = (this.aCButtons[i].offsetLeft + this.aCButtons[i].offsetWidth / 2) + "px";
         }
     }
 
@@ -36,19 +49,27 @@ export class NewsComponent{
     }
 
     underscoreMove(nav: any): void {
+        let aButtons, aUnderscores;
+        if(nav.dataset.id.toUpperCase() === "NEWS") {
+            aButtons = this.aButtons;
+            aUnderscores = this.aUnderscores;
+        }else if(nav.dataset.id.toUpperCase() === "CAREER") {
+            aButtons = this.aCButtons;
+            aUnderscores = this.aCUnderscores;
+        }
         if(nav.className.toUpperCase() !== "ACTIVE") {
             let index = nav.dataset.index;
-            for(let i=0;i<this.aButtons.length;i++) {
-                this.aButtons[i].className = "";
-                this.appUtil.myMove_yzy(this.aUnderscores[i],{
+            for(let i=0;i<aButtons.length;i++) {
+                aButtons[i].className = "";
+                this.appUtil.myMove_yzy(aUnderscores[i],{
                     width: 0,
-                    left: (this.aButtons[i].offsetLeft + this.aButtons[i].offsetWidth / 2)
+                    left: (aButtons[i].offsetLeft + aButtons[i].offsetWidth / 2)
                 })
             }
-            this.aButtons[index-1].className = "active";
-            this.appUtil.myMove_yzy(this.aUnderscores[index-1],{
-                width: this.aButtons[index-1].offsetWidth,
-                left: this.aButtons[index-1].offsetLeft
+            aButtons[index-1].className = "active";
+            this.appUtil.myMove_yzy(aUnderscores[index-1],{
+                width: aButtons[index-1].offsetWidth,
+                left: aButtons[index-1].offsetLeft
             });
         }
     }
